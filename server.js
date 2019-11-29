9df01c9f4b4aa4e418b90494779986bc1cb2e766
tampc <tamphanpw1@gmail.com>
2019-11-29 16:39:02 +0700

    fixed bugs

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/MyAngularApp'));
+app.use(express.static(__dirname + '/dist/ngx-admin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/MyAngularApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ngx-admin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
