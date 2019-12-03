634bfae9a0f1bd77b0eea8132b1e9d5186448088
tampc <tamphanpw1@gmail.com>
2019-12-03 14:36:54 +0700

    modified production url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   limit: 12,
   relativeImg_limit: 20,
-  apiUrl: 'https://tp-shop-api.herokuapp.com/api',
+  apiUrl: 'https://tapha-shop-api.herokuapp.com/api',
   logo: 'TAPHA'
   // apiUrl: 'http://192.168.1.59:3000'
 };
