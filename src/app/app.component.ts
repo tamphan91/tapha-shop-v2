737d6f9aaf2c33e1929bec2212f9806016150843
tampc <tamphanpw1@gmail.com>
2019-12-05 14:26:30 +0700

    call api awake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,17 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
     selector: 'ngx-app',
     template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-    constructor(private analytics: AnalyticsService) {
-        
+    constructor(private analytics: AnalyticsService, private http: HttpClient) {
+        // call api awake
+        this.http.get(environment.apiUrl).subscribe();
     }
 
     ngOnInit(): void {
